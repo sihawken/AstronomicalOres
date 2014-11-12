157c2af0b0840b1621b84a41eb5f91a3bb408014
Tim Shewmaker, Jr <xchainblade@gmail.com>
2014-11-12 00:59:34 -0600

    version update for 1.0.2

--- src/main/java/com/zerren/interstellarores/reference/Reference.java
+++ src/main/java/com/zerren/interstellarores/reference/Reference.java
@@ -6,7 +6,7 @@ package com.zerren.interstellarores.reference;
 public class Reference {
     public static final String MOD_ID = "InterstellarOres";
     public static final String MOD_NAME = "Interstellar Ores";
-    public static final String VERSION = "1.7.10-1.0.0";
+    public static final String VERSION = "1.7.10-1.0.2";
     public static final String CLIENTPROXY_CLASS = "com.zerren.interstellarores.proxy.ClientProxy";
     public static final String SERVERPROXY_CLASS = "com.zerren.interstellarores.proxy.ServerProxy";
     public static final String GUIFACTORY_CLASS = "com.zerren.interstellarores.client.gui.GuiFactory";
